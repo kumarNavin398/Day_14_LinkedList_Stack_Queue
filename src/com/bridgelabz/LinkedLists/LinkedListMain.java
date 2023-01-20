5b4c71ee77e28d14db0d77964cbc14157ca927e0
KumarNavin <navins398@gmail.com>
2023-01-20 19:41:40 +0530

    searchElement

--- src/com/bridgelabz/LinkedLists/LinkedListMain.java
+++ src/com/bridgelabz/LinkedLists/LinkedListMain.java
@@ -1,23 +1,31 @@
 package com.bridgelabz.LinkedLists;
 
+
 public class LinkedListMain {
     public static void main(String[] args) {
+        //uc2
         LinkedList<Integer> linkedList1 = new LinkedList<>();
         linkedList1.push(70);
         linkedList1.push(30);
         linkedList1.push(56);
-        linkedList1.append(89);
         linkedList1.display();
 
-        System.out.println("After pop");
+        //uc3
+        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        linkedList2.append(56);
+        linkedList2.append(30);
+        linkedList2.append(70);
+        linkedList2.display();
+
+        //uc5
         linkedList1.pop();
-
         linkedList1.display();
 
-        System.out.println("After popLast");
-        linkedList1.popLast();
+        //uc6
+        linkedList2.popLast();
+        linkedList2.display();
 
-        linkedList1.display();
-
+        //uc7
+        linkedList2.search(30);
     }
 }
