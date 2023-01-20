c7d240e0643a2fa822fb3d7ce5d0d91ef16422b3
KumarNavin <navins398@gmail.com>
2023-01-20 15:39:33 +0530

    createdLinkedList

--- src/com/bridgelabz/LinkedLists/LinkedListMain.java
+++ src/com/bridgelabz/LinkedLists/LinkedListMain.java
@@ -2,6 +2,10 @@ package com.bridgelabz.LinkedLists;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.push(70);
+        linkedList1.push(30);
+        linkedList1.push(56);
+        linkedList1.display();
     }
 }
